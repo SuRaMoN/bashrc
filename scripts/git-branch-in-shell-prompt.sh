2dab8b0706d3bcc367968f51e6bb0e96e82aa4ad
matthias <matthias@icontroller.be>
2013-04-11 21:25:16 +0200

    Added random color to hostname

--- scripts/git-branch-in-shell-prompt.sh
+++ scripts/git-branch-in-shell-prompt.sh
@@ -4,10 +4,19 @@ parse_git_branch() {
 	git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \[\1\]/'
 }
 
+get_hostname_in_color_by_hash() {
+	local i HASH
+	HASH=0
+	for (( i=0; i< ${#HOSTNAME}; i++ )); do
+	  ((HASH+=$(printf '%d' "'${HOSTNAME:$i:1}'")))
+	done
+	echo -e "\033[$(($HASH%2));$((30+($HASH/2)%8))m$HOSTNAME\033[00m"
+}
+
 if [[ ${EUID} == 0 ]] ; then
-	PS1='\[\033[01;31m\]\u@\h\[\033[01;35m\]$(parse_git_branch)\[\033[01;34m\] \[\033[01;32m\]${PWD/$HOME/~}\[\033[00m\]\n\$ \[\033[00m\]'
+	PS1='\n$(get_hostname_in_color_by_hash) \[\033[01;31m\]\u\[\033[01;35m\]$(parse_git_branch)\[\033[01;34m\] \[\033[01;32m\]${PWD/$HOME/~}\[\033[00m\]\n\$ \[\033[00m\]'
 else
-	PS1='\[\033[01;31m\]\u@\h\[\033[00;33m\]$(parse_git_branch)\[\033[00m\] \[\033[01;32m\]${PWD/$HOME/~}\[\033[00m\]\n\$ '
+	PS1='\n$(get_hostname_in_color_by_hash) \[\033[01;31m\]\u\[\033[00;33m\]$(parse_git_branch)\[\033[00m\] \[\033[01;32m\]${PWD/$HOME/~}\[\033[00m\]\n\$ '
 fi
 
 # Change the window title of X terminals
